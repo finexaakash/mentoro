b8552655635280fb5c06a0b72752acb1e9f40874
finexaakash <aakashsaini0998@gmail.com>
2026-03-23 20:30:49 +0530

    fix build error

--- src/appwrite/auth.js
+++ src/appwrite/auth.js
@@ -24,7 +24,19 @@ export class AuthService {
 
     }
 
-  
+  // async login({ email, password }) {
+  //   try {
+  //     const session = await this.account.createEmailPasswordSession(email, password);
+
+  //     // 🔥 Force session sync
+  //     await this.account.get();
+
+  //     return session;
+  //   } catch (error) {
+  //     console.log("Login Error:", error);
+  //     throw error;
+  //   }
+  // }
   async login({ email, password }) {
   try {
     await this.account.createEmailPasswordSession(email, password);
@@ -77,5 +89,96 @@ const authService = new AuthService();
 export default authService;
 
 
- if (!userAccount) throw new Error("Account not created");
 
+// import conf from "../conf/conf.js";
+// import { Client, Account, ID } from "appwrite";
+
+// export class AuthService {
+//   client = new Client();
+//   account;
+
+//   constructor() {
+//     this.client
+//       .setEndpoint(conf.appwriteUrl)
+//       .setProject(conf.projectId);   // ✅ fixed
+
+//     this.account = new Account(this.client);
+//   }
+
+//   async createAccount({ email, password, name }) {
+//     try {
+//       const userAccount = await this.account.create(
+//         ID.unique(),
+//         email,
+//         password,
+//         name
+//       );
+
+//       if (!userAccount) throw new Error("Account not created");
+
+//       await this.login({ email, password });
+
+//       return userAccount;
+//     } catch (error) {
+//       console.log("Create Account Error:", error);
+//       throw error;
+//     }
+//   }
+
+//   async login({ email, password }) {
+//     try {
+//       return await this.account.createEmailPasswordSession(email, password);
+//     } catch (error) {
+//       console.log("Login Error:", error);
+//       throw error;
+//     }
+//   }
+
+//   async getCurrentUser() {
+//     try {
+//       return await this.account.get();
+//     } catch {
+//       return null;
+//     }
+//   }
+
+//   async isLoggedIn() {
+//     try {
+//       const user = await this.account.get();
+//       return !!user;
+//     } catch {
+//       return false;
+//     }
+//   }
+
+//   async updateName(name) {
+//     try {
+//       return await this.account.updateName(name);
+//     } catch (error) {
+//       console.log("Update Name Error:", error);
+//       throw error;
+//     }
+//   }
+
+//   async updatePassword(oldPassword, newPassword) {
+//     try {
+//       return await this.account.updatePassword(newPassword, oldPassword);
+//     } catch (error) {
+//       console.log("Update Password Error:", error);
+//       throw error;
+//     }
+//   }
+
+//   async logout() {
+//     try {
+//       await this.account.deleteSession("current"); // ✅ improved
+//       return true;
+//     } catch (error) {
+//       console.log("Logout Error:", error);
+//       return false;
+//     }
+//   }
+// }
+
+// const authService = new AuthService();
+// export default authService;
